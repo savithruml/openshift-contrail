2bdeee75d8792fa59f1ae6be5c4bc767fe5c80cb
root <root@ip-10-0-35-154.us-west-2.compute.internal>
2017-06-12 03:08:05 +0000

    Adding scripts

---
+++ init/enable-root-password.sh
@@ -0,0 +1,15 @@
+# Author: SAVITHRU LOKANATH
+# Contact: SAVITHRU AT JUNIPER.NET
+# Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+
+#!/bin/bash
+
+sudo su
+passwd
+sed -e 's/#PermitRootLogin yes/PermitRootLogin yes/g' -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config 
+service sshd restart
+yum install git wget -y
+cd /root/
+git clone https://github.com/savithruml/openshift-contrail
+
+echo "Successfully updated root password. Try logging into the instance as root user"
